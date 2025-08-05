d0f8a998977919db28f18b178dea76931d056e8b
kralion <joan300501@gmail.com>
2025-08-05 14:37:25 -0500

    Migration to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,20 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://grobles.vercel.app',
+  integrations: [mdx(), sitemap()],
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
