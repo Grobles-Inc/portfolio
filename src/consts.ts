c1588b77905e970faa740b973c74e6bc65baded2
kralion <joan300501@gmail.com>
2025-08-07 10:47:54 -0500

    exporting consts fixed

--- src/consts.ts
+++ src/consts.ts
@@ -2,12 +2,12 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Grobles";
-let SITE_DESCRIPTION = "Startup líder en desarrollo de software";
-const currentLocale = "es";
-
-if (currentLocale === "es") {
-  SITE_DESCRIPTION = "Startup líder en desarrollo de software";
-} else {
-  SITE_DESCRIPTION = "Startup leader in software development";
-}
+export const SITE_DESCRIPTION = (() => {
+  const currentLocale = "es";
+  if (currentLocale === "es") {
+    return "Startup líder en desarrollo de software";
+  } else {
+    return "Startup leader in software development";
+  }
+})();
 
